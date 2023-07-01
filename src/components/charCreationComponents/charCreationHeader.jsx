0276665e991db3e0c791428cff91734a43abe4d5
Vyshnevetskyi Pasha <demonaft1@gmail.com>
2023-07-01 20:07:27 +0300

    refactor

--- src/components/charCreationComponents/charCreationHeader.jsx
+++ src/components/charCreationComponents/charCreationHeader.jsx
@@ -1,41 +1,26 @@
 import React from "react";
-import LabelInput from "./labelInput";
+import LabelInput from "./subParts/labelInput";
+import GridItem from "./subParts/gridItem";
 
 function CharCreationHeader() {
-    return <div className="wrapper">
-    <div className="header char-name container">
-    <div className="vert-center">
-         <div>
-            <LabelInput labelFor="char-name" labelText="Character name" inputSize="8" inputType="text" inputName="char-name" inputId="char-name"/>
-        </div>
-        <div>
-           <LabelInput labelFor="char-level" labelText="Character level" inputSize="8" inputType="text" inputName="char-level" inputId="char-level"/>
-        </div>
-        </div>
-    </div>
-    <div className="header stats container">
-     <div className="header stats grid">
-     <div className="grid-item">
-        <LabelInput labelFor="class-level" labelText="Class" inputSize="8" inputType="text" inputName="class-level" inputId="class-level" br=<br/>/>
-     </div>
-     <div className="grid-item">
-        <LabelInput labelFor="background" labelText="Background" inputSize="8" inputType="text" inputName="background" inputId="background" br=<br/>/>
-     </div>
-     <div className="grid-item">
-       <LabelInput labelFor="player-name" labelText="Player Name" inputSize="8" inputType="text" inputName="player-name" inputId="player-name" br=<br/>/>
-     </div>
-     <div className="grid-item">
-       <LabelInput labelFor="race" labelText="Race" inputSize="8" inputType="text" inputName="race" inputId="race" br=<br/>/>
-     </div>
-     <div className="grid-item">
-       <LabelInput labelFor="alignment" labelText="Alignment" inputSize="8" inputType="text" inputName="alignment" inputId="alignment" br=<br/>/>
-     </div>
-     <div className="grid-item">
-      <LabelInput labelFor="experience" labelText="Class" inputSize="8" inputType="text" inputName="experience" inputId="experience" br=<br/>/>
-     </div>
-        </div>
-    </div>
-    </div>
+   return <div className="wrapper">
+      <div className="header char-name container">
+         <div className="vert-center">
+            <LabelInput labelFor="char-name" labelText="Character name" inputSize="8" inputType="text" inputName="char-name" inputId="char-name" />
+            <LabelInput labelFor="char-level" labelText="Character level" inputSize="8" inputType="text" inputName="char-level" inputId="char-level" />
+         </div>
+      </div>
+      <div className="header stats container">
+         <div className="header stats grid">
+            <GridItem labelFor="class-level" labelText="Class" inputSize="8" inputType="text" inputName="class-level" inputId="class-level" br=<br /> />
+            <GridItem labelFor="background" labelText="Background" inputSize="8" inputType="text" inputName="background" inputId="background" br=<br /> />
+            <GridItem labelFor="player-name" labelText="Player Name" inputSize="8" inputType="text" inputName="player-name" inputId="player-name" br=<br /> />
+            <GridItem labelFor="race" labelText="Race" inputSize="8" inputType="text" inputName="race" inputId="race" br=<br /> />
+            <GridItem labelFor="alignment" labelText="Alignment" inputSize="8" inputType="text" inputName="alignment" inputId="alignment" br=<br /> />
+            <GridItem labelFor="experience" labelText="Class" inputSize="8" inputType="text" inputName="experience" inputId="experience" br=<br /> />
+         </div>
+      </div>
+   </div>
 }
 
 export default CharCreationHeader;
---
+++ src/components/charCreationComponents/subParts/gridItem.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function GridItem(props) {
+    return <div className="grid-item">
+        <label htmlFor={props.labelFor}>{props.labelText}</label>
+        {props.br}
+        <input size={props.inputSize} type={props.inputType} name={props.inputName} id={props.inputId}></input>
+    </div>
+}
+
+export default GridItem;
