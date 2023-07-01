14de5c4938a29ca4d28f9849285602d1effff203
Vyshnevetskyi Pasha <demonaft1@gmail.com>
2023-07-01 21:26:04 +0300

    cosmetic fix

--- src/components/charCreationComponents/subParts/mainStat.jsx
+++ src/components/charCreationComponents/subParts/mainStat.jsx
@@ -5,13 +5,13 @@ function Stat(props) {
     const [inc, setInc] = useState("0");
 
     function getBonus(event) {
-        setInc(Math.min(Math.floor((Number(event.target.value) / 2) - 5), 10));
+        setInc(Math.max(Math.min(Math.floor((Number(event.target.value) / 2) - 5), 10), -5));
     }
 
     return (<div className="main-stat container">
         <label htmlFor={props.stat}>{props.stat}</label>
         <input type="number" onChange={getBonus} name={props.stat} id={props.stat}></input>
-        <p className="stat-bonus">{inc}</p>
+        <p className="stat-bonus">{inc > 0 && "+"}{inc}</p>
     </div>)
 }
 
