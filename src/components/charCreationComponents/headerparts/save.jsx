590681c0767a43a04b9bd9d9b185ec50a4aab0ae
Vyshnevetskyi Pasha <demonaft1@gmail.com>
2023-07-09 17:05:22 +0300

    add .env, started save impl

--- src/components/charCreationComponents/headerparts/save.jsx
+++ src/components/charCreationComponents/headerparts/save.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 
 function saveCharacter() {
-    console.log("char saved");
+
+    const requestBody = {
+        name: document.getElementById('char-name').value,
+        level: document.getElementById('char-level').value,
+        charClass: document.getElementById('class').value,
+        race: document.getElementById('race').value,
+        alignment: document.getElementById('alignment').value,
+        background: document.getElementById('background').value
+        //todo playerName and experience
+    }
+
+    fetch(process.env.REACT_APP_API_URL + "/character", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(requestBody)
+    }).then();
 }
 
 function Save() {
